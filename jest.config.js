dddc323b0f334db0074fdfee99b830ce8061d7b4
ankita yadav <ankita.yadav2906@gmail.com>
2022-09-29 19:35:20 +0530

    new code

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  transformIgnorePatterns: ["/node_modules/(?!@meforma/vue-toaster)"],
-
-}
